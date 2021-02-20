12eb1c28e07deac05e58fbcc406961055fc0e04f
Kenny <kenny_oliver@icloud.com>
2021-02-20 16:46:28 +0000

    [+] Setup Bash [+] Create main program

---
+++ main.py
@@ -0,0 +1,31 @@
+def bubble_sort(data):
+  length = len(data) - 1
+  sorted = False
+  
+  while not sorted:
+    sorted = True
+    for i in range(length):
+      if data[i] > data[i+1]:
+        data[i+1],data[i] = data[i],data[i+1]
+        sorted = False
+  return data
+#====================
+def binary_search(structure,query):
+  start = 0
+  end = len(structure) - 1
+  
+  while start <= end:
+    midpoint = (start + end) // 2
+    if structure[midpoint] == query:
+      return midpoint
+    else:
+      if structure[midpoint] < query:
+        midpoint = end + 1
+      else:
+        midpoint = start - 1
+    
+  return False
+#====================
+# MAIN PROGRAM
+arr = [1,5,2,4,3]
+print(binary_search(bubble_sort(arr),2))
